ff8f6503a186fe840c6b5f1cddbcd5d38d79a083
denchdudarev <dudarev94@gmail.com>
2022-06-06 23:16:50 +0300

    02

---
+++ app/src/components/productsList/productsList.vue
@@ -0,0 +1,57 @@
+<template>
+    <ul class="catalog__list">
+          <li class="catalog__item">
+            <a class="catalog__pic" href="#">
+              <img src="img/radio.jpg" srcset="img/radio@2x.jpg 2x" alt="Название товара">
+            </a>
+
+            <h3 class="catalog__title">
+              <a href="#">
+                Радионяня Motorola MBP16
+              </a>
+            </h3>
+
+            <span class="catalog__price">
+              3 690 ₽
+            </span>
+
+            <ul class="colors colors--black">
+              <li class="colors__item">
+                <label class="colors__label">
+                  <input class="colors__radio sr-only" type="radio" name="color-1" value="#73B6EA" checked="">
+                  <span class="colors__value" style="background-color: #73B6EA;">
+                  </span>
+                </label>
+              </li>
+              <li class="colors__item">
+                <label class="colors__label">
+                  <input class="colors__radio sr-only" type="radio" name="color-1" value="#8BE000">
+                  <span class="colors__value" style="background-color: #8BE000;">
+                  </span>
+                </label>
+              </li>
+              <li class="colors__item">
+                <label class="colors__label">
+                  <input class="colors__radio sr-only" type="radio" name="color-1" value="#222">
+                  <span class="colors__value" style="background-color: #222;">
+                  </span>
+                </label>
+              </li>
+            </ul>
+          </li>
+        </ul>
+</template>
+
+<script>
+import products from 'app/src/data/products.js'
+
+    export default {
+      name: 'productsList',
+      data() {
+        return {
+          products,
+        }
+      },
+    }
+</script>
+ 
---
+++ app/src/data/products.js
@@ -0,0 +1,7 @@
+export default [
+    {
+        title: 'Радионяня Motorola MBP16',
+        price: 3690,
+        img: 'img/radio.jpg',
+    },
+]
