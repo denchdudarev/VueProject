5b6b57b3027a68a0940a1f4f92349d93b3ebf29e
denchdudarev <dudarev94@gmail.com>
2022-06-07 16:01:12 +0300

    03

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,27 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="catalog">
+          <ul class="catalog__list">
+            <li class="catalog__item" v-for="{product, index} in products" :key="index">
+              <a class="catalog__pic" href="#">
+                <img :src="product.img" alt="Название товара">
+              </a>
+  
+              <h3 class="catalog__title">
+                <a href="#">
+                  {{ product.title }}
+                </a>
+              </h3>
+  
+              <span class="catalog__price">
+                {{ product.price }} ₽
+              </span>
+  
+              <ul class="colors colors--black">
+                <li class="colors__item">
+                  <label class="colors__label">
+                    <input class="colors__radio sr-only" type="radio" name="color-1" value="#73B6EA" checked="">
+                    <span class="colors__value" style="background-color: #73B6EA;">
+                    </span>
+                  </label>
+                </li>
+                <li class="colors__item">
+                  <label class="colors__label">
+                    <input class="colors__radio sr-only" type="radio" name="color-1" value="#8BE000">
+                    <span class="colors__value" style="background-color: #8BE000;">
+                    </span>
+                  </label>
+                </li>
+                <li class="colors__item">
+                  <label class="colors__label">
+                    <input class="colors__radio sr-only" type="radio" name="color-1" value="#222">
+                    <span class="colors__value" style="background-color: #222;">
+                    </span>
+                  </label>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import products from './data/products'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
+  components: products,
+  data() {
+    return {
+      products
+    }
   },
 };
 </script>
-
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
-</style>
--- app/src/data/products.js
+++ app/src/data/products.js
@@ -2,6 +2,11 @@ export default [
     {
         title: 'Радионяня Motorola MBP16',
         price: 3690,
-        img: 'img/radio.jpg',
+        img: 'img/phone-2.jpg',
     },
+    {
+        title: 'Scooter',
+        price: 10000,
+        img: 'img/airpods.jpg',
+    }
 ]
