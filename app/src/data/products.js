f5f77b7de237d23602ba1c1fc198f533465f4d9b
denchdudarev <dudarev94@gmail.com>
2022-06-21 12:03:09 +0300

    08

--- app/src/data/products.js
+++ app/src/data/products.js
@@ -1,60 +1,72 @@
 export default [
     {
+        id: 1,
         title: 'Skateboard1',
         price: 3690,
         img: 'img/board.jpg'
     },
     {
+        id: 2,
         title: 'Air Pods1',
         price: 10000,
         img: '/img/airpods.jpg'
     },
     {
+        id: 3,
         title: 'Phone1',
         price: 30000,
         img: '/img/phone-1@2x.jpg'
     },
     {
+        id: 4,
         title: 'Skateboard2',
         price: 3690,
         img: 'img/board.jpg'
     },
     {
+        id: 5,
         title: 'Air Pods2',
         price: 10000,
         img: '/img/airpods.jpg'
     },
     {
+        id: 6,
         title: 'Phone2',
         price: 30000,
         img: '/img/phone-1@2x.jpg'
     },
     {
+        id: 7,
         title: 'Skateboard3',
         price: 3690,
         img: 'img/board.jpg'
     },
     {
+        id: 8,
         title: 'Air Pods3',
         price: 10000,
         img: '/img/airpods.jpg'
     },
     {
+        id: 9,
         title: 'Phone3',
         price: 30000,
         img: '/img/phone-1@2x.jpg'
     },
     {
+        id: 10,
         title: 'Skateboard4',
         price: 3690,
         img: 'img/board.jpg'
     },
     {
+        id: 11,
         title: 'Air Pods4',
         price: 10000,
         img: '/img/airpods.jpg'
     },
     {
+        id: 12,
         title: 'Phone4',
         price: 30000,
         img: '/img/phone-1@2x.jpg'
